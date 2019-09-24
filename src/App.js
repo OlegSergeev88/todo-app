ca5277e85382c7cae7d3e48c9634337a5ec1ec2a
Oleg Sergeev <osergeev1337@gmail.com>
2019-09-24 21:21:12 +0300

    Add footer and header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
 import Create from "./components/create";
+import Footer from "./components/footer";
+import Header from "./components/header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Create />
+      <Footer />
     </div>
   );
 }
